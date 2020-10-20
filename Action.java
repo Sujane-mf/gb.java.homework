d1db15b7b7b8fc178f50110bb02d7320576d04ec
Sujane-mf <70478395+Sujane-mf@users.noreply.github.com>
2020-10-20 23:31:20 +0300

    Add files for homework1 J2

---
+++ Action.java
@@ -0,0 +1,6 @@
+package ru.geekbrains.j2_homework1;
+
+public interface Action {
+    public boolean run (Obstacle obs);
+    public void jump (Obstacle obs);
+}
---
+++ Robot.java
@@ -0,0 +1,26 @@
+package ru.geekbrains.j2_homework1;
+
+public class Robot implements Action{
+    int maxlength, maxheight; //ограничения на длину забега и высоту прыжка
+    public Robot (int maxlength, int maxheight){
+        this.maxlength = maxlength;
+        this.maxheight = maxheight;
+    }
+    public boolean run (Obstacle obs){
+        if (obs instanceof Treadmill && maxlength >= ((Treadmill) obs).size){
+            System.out.println("Робот пробежал дорожку ");
+            return true;
+        } else {System.out.println("Робот не смог пробежать дорожку, его ограничения " + maxlength);
+            return false;
+        }
+    }
+
+    public void jump(Obstacle obs) {
+        if (obs instanceof Wall && maxheight >= ((Wall) obs).size) {
+            System.out.println("Робот перепрыгнул стену ");
+
+        }else {System.out.println("Робот не смог перепрыгунть стену, его ограничения " + maxheight);
+
+        }
+    }
+}
