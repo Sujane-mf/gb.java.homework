8d04518f12b8fbd02a773e3a19e895db20ce6f81
sujane <sujane_mf@mail.ru>
2020-11-02 18:15:33 +0300

    исправление дз в части main

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,29 +1,35 @@
 package ru.geekbrains.j2_homework3;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class PhoneBook {
-    private HashMap<String, String> card;
+    private HashMap<String, Set<String>> card;
 
-    public PhoneBook(HashMap<String, String> card) {
+    public PhoneBook(HashMap<String, Set<String>> card) {
         this.card = card;
     }
 
-    public void addCard(String number, String surname) throws WrongKeyException {
-        if (!card.containsKey(number)) {
-            card.put(number, surname);
+    public void addCard(String surname, String number) {
+        if (!card.containsKey(surname)) {
+            Set<String> numbers = new HashSet<>();
+            numbers.add(number);
+            card.put(surname, numbers);
         } else {
-            throw new WrongKeyException("Такой номер телефона уже есть " + number);
+            card.get(surname).add(number);
         }
     }
 
-    public void getNumber(String surname) {
-        for (HashMap.Entry<String, String> entry : card.entrySet()) {
-            if (entry.getValue().equals(surname)) {
-                System.out.println("Телефон сотрудника " + surname + ": " + entry.getKey());
+    public Set<String> getNumber(String surname) {
+        if (card.get(surname) == null){
+            return new HashSet<>();
+        }
+
+        return card.get(surname);
+
             }
         }
-    }
-}
 
 
